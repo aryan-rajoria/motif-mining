f83c1b2cd58a5a511d5bdbc79df2dcb13b624fce
Aryan Rajoria <aryanrajoria1003@gmail.com>
2023-03-07 01:53:15 -0500

    mining results

--- mining_results.py
+++ mining_results.py
@@ -15,39 +15,38 @@ import sqlite3
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
-
 model = models.resnet50(pretrained=True, progress=False)
 for param in model.parameters():
     param.requires_grad = False
 model.fc = torch.nn.Identity()
 model.to(device)
 model.eval()
-print('', end='')
 
 def transform(images: np.ndarray):
     transformed = [transforms.ToTensor()]
     composed = Compose(transformed)
     return composed(Image.fromarray(images[:, :, ::-1])).unsqueeze(0)
+
 def as_numpy(val: Tensor) -> np.ndarray:
-        return val.detach().cpu().numpy()
+    return val.detach().cpu().numpy()
 
 
 def model_output(image_path):
     img = cv2.imread(image_path)
     imgt = transform(img)
-    # f = (3, width, height) values: 0-1
     imgt = imgt.to(device)
     with torch.no_grad():
         inference = as_numpy(model(torch.unsqueeze(imgt[0], 0)))
     return inference
 
 
-INDEX_PATH =  "/nethome/kravicha3/aryan/project/notebooks/redditdataset/HNSW_dataindex.index"
+INDEX_PATH =  "/nethome/kravicha3/aryan/project/notebooks/redditdataset/saves/HNSW_dataindex.index"
 index = faiss.read_index(INDEX_PATH)
 k = 10
 REDDIT_DATA_HOME = os.path.join("/nethome/kravicha3/aryan/project/dataset/Reddit_Provenance_Datasets/data/")
 reddit_threads = os.listdir(REDDIT_DATA_HOME)
-c = sqlite3.connect("/home/kravicha3/.eva/0.1.5+dev/eva_catalog.db").cursor()
+# reddit_threads = ['_This_picture_of_Hillary_Clinton_and_Barack_Obama']
+c = sqlite3.connect("/nethome/kravicha3/aryan/project/notebooks/redditdataset/saves/eva_catalog.db").cursor()
 
 def get_similarity_results(image_path):
     # get model output
@@ -63,19 +62,19 @@ def get_similarity_results(image_path):
     return results
 
 
-
 def get_examples():
     dataframe = pd.DataFrame(columns=['image_name', 'image_dir', 'num_similar', 'results'])
     # take a directory and get all image results from it
     for dir_name in reddit_threads:
         img_dir = os.path.join(REDDIT_DATA_HOME, dir_name)
-        for file in os.listdir(img_dir):
+        for file_name in os.listdir(img_dir):
             # store and save results some where
-            if (file.endswith(".jpg") or file.endswith(".png")):
-                fp = os.path.join(img_dir, file)
+            if (file_name.endswith(".jpg") or file_name.endswith(".png")):
+                fp = os.path.join(img_dir, file_name)
                 try:
                     result = get_similarity_results(fp)
-                except:
+                except Exception as e:
+                    print(e)
                     continue
                 num_sim = 0
                 
@@ -84,8 +83,13 @@ def get_examples():
                     if img_dir in name:
                         num_sim += 1
                 
-                dataframe.loc[len(dataframe)] = [file, img_dir, num_sim, result]
+                dataframe.loc[len(dataframe)] = [file_name, img_dir, num_sim, result]
+                torch.cuda.empty_cache()
+            
+        print(f"completed dir: {dir_name}", len(dataframe))
+
     return dataframe
 
 output_frame = get_examples()
+print(len(output_frame))
 output_frame.to_pickle("hnsw_all_image_results.pkl")
